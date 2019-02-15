0ba5980d07ff1a7518435eeed2c6bdddd66bfef8
Joshua Hogendorn <josh.hogendorn@origin.com.au>
2019-02-15 11:46:32 +1000

    adds pipefails to init

--- lib/init.sh
+++ lib/init.sh
@@ -2,7 +2,7 @@
 
 [ -n "$DEBUG" ] && set -x # Print traces when things go wrong in debug mode
 
-set -eu # -e If a command fails, stop execution
-        # -u Fail if we try to expand unset variables
-
+set -eu         # -e If a command fails, stop execution
+                # -u Fail if we try to expand unset variables
+set -o pipefail # Fail even on pipes
 # }}}

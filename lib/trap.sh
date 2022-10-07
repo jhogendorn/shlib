0ffe5f170f68432b976761ae5feb167e4789aaeb
Joshua Hogendorn <jhogendorn@gmail.com>
2022-10-07 16:38:35 +1000

    [feat] trap: refactor and cleanup
    Curry signals and trap EXIT better.

--- lib/trap.sh
+++ lib/trap.sh
@@ -1,6 +1,8 @@
 # Ensure clean exits {{{
 
-_finish () {
+_trap () {
+  sig=${1} ; shift
+
   if [ -n "$(type -t _cleanup)" ] && [ "$(type -t _cleanup)" = function ]; then
     _cleanup
   fi
@@ -8,14 +10,21 @@ _finish () {
   # Propagate the exit signal correctly
   if [ $sig != EXIT ]
   then
-      trap - $sig EXIT
-      kill -s $sig $$
+      trap '' EXIT # some shells will call exit after INT handler
+      kill -s $sig $$ # ensure the script ends with the right signal
   fi
 
-  exit
+  exit $?
 }
-export -f _finish
 
-trap _finish INT TERM EXIT
+_trap_init() {
+  func="$1" ; shift
+  for sig in "$@" ; do
+    #echo "trapping $sig"
+    trap "$func $sig" "$sig"
+  done
+}
+
+_trap_init _trap INT TERM EXIT
 
 # }}}
